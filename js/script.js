7ca76ac33cc18a10dd589c270be1ca31cca07552
Juancapp <juan.cappuccio@radiumrocket.com>
2023-08-04 16:57:00 -0300

    PACMAN: implement functions to move pacman and collision with the ghost

---
+++ js/script.js
@@ -0,0 +1,52 @@
+var pacman;
+var ghost;
+
+window.addEventListener("DOMContentLoaded", function () {
+  pacman = document.getElementById("pacman");
+  ghost = document.getElementById("ghost");
+  console.log(pacman);
+  console.log(ghost);
+});
+
+function elementsOverlap(el1, el2) {
+  const domRect1 = el1.getBoundingClientRect();
+  const domRect2 = el2.getBoundingClientRect();
+
+  return !(
+    domRect1.top > domRect2.bottom ||
+    domRect1.right < domRect2.left ||
+    domRect1.bottom < domRect2.top ||
+    domRect1.left > domRect2.right
+  );
+}
+
+function move(event) {
+  switch (event.keyCode) {
+    case 38:
+      var currentPosition = parseInt(getComputedStyle(pacman).top);
+      pacman.style.top = currentPosition - 5 + "px";
+      break;
+    case 40:
+      var currentPosition = parseInt(getComputedStyle(pacman).top);
+      pacman.style.top = currentPosition + 5 + "px";
+      break;
+    case 37:
+      var currentPosition = parseInt(getComputedStyle(pacman).left);
+      pacman.style.left = currentPosition - 5 + "px";
+      break;
+    case 39:
+      var currentPosition = parseInt(getComputedStyle(pacman).left);
+      pacman.style.left = currentPosition + 5 + "px";
+      break;
+  }
+
+  if (elementsOverlap(pacman, ghost)) {
+    alert("You lost");
+    pacman.style = {
+      top: 0,
+      left: 0,
+    };
+  }
+}
+
+document.addEventListener("keydown", move);
